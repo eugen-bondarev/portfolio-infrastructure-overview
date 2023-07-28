c5cae2b53340f8241aca11fa781a762aeb10d329
Eugen Bondarev <eug.bondarev@gmail.com>
2023-07-28 17:28:13 +0200

    Some progress, version updated to 1.0.2

--- src/app/components/Node/index.tsx
+++ src/app/components/Node/index.tsx
@@ -1,14 +1,23 @@
 import Image from 'next/image'
+import clsx from 'clsx'
 
 interface NodeProps {
+  className?: string
   icon?: string
   title?: string
   description?: string
+  id?: string
 }
 
-const Node = ({ icon, title, description }: NodeProps) => {
+const Node = ({ className, id, icon, title, description }: NodeProps) => {
   return (
-    <div className="bg-slate-200 p-8 flex flex-col rounded-xl text-slate-900 max-w-[25rem] gap-4">
+    <div
+      id={id}
+      className={clsx(
+        className,
+        'bg-slate-200 p-8 flex flex-col rounded-xl text-slate-900 w-[400px] gap-4'
+      )}
+    >
       <div className="flex gap-4 items-center h-10">
         {icon ? <Image src={icon} width={48} height={48} alt="Icon" /> : null}
         <h1 className="font-bold text-2xl">{title}</h1>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,108 @@
+'use client'
 import Node from './components/Node'
+import { useLayoutEffect, useState } from 'react'
+
+const useElements = ({
+  fromSelector,
+  toSelector,
+}: {
+  fromSelector: string
+  toSelector: string
+}) => {
+  const [from, setFrom] = useState<HTMLElement | undefined>()
+  const [to, setTo] = useState<HTMLElement | undefined>()
+
+  useLayoutEffect(() => {
+    if (from && to) {
+      return
+    }
+    const timeoutId = setTimeout(() => {
+      setFrom(
+        (document.querySelector(fromSelector) as HTMLElement) || undefined
+      )
+      setTo((document.querySelector(toSelector) as HTMLElement) || undefined)
+    }, 10)
+    return () => clearTimeout(timeoutId)
+  }, [fromSelector, toSelector])
+
+  return { from, to }
+}
+
+interface ConnectionProps {
+  from: string
+  to: string
+}
+
+const toDeg = (rad: number) => (rad * 180) / Math.PI
+
+const Connection = ({
+  from: fromSelector,
+  to: toSelector,
+}: ConnectionProps) => {
+  const { from, to } = useElements({ fromSelector, toSelector })
+
+  if (!from || !to) {
+    return
+  }
+
+  const start = {
+    x: from.offsetLeft + from.clientWidth / 2,
+    y: from.offsetTop + from.clientHeight,
+  }
+
+  const end = {
+    x: to.offsetLeft + to.clientWidth / 2,
+    y: to.offsetTop,
+  }
+
+  const angleRad = Math.atan2(end.y - start.y, end.x - start.x)
+  const angle = -toDeg(angleRad)
+  const rotation = angle === -90 ? 0 : angle
+
+  const length = Math.sqrt(
+    Math.pow(end.x - start.x, 2) + Math.pow(end.y - start.y, 2)
+  )
+
+  return (
+    <div
+      className="absolute w-1 bg-slate-200"
+      style={{
+        left: `${start.x}px`,
+        top: `${start.y}px`,
+        height: `${length}px`,
+        transform: `rotate(${rotation}deg)`,
+      }}
+    ></div>
+  )
+}
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex items-center p-20 flex-col gap-[4rem]">
+      <Connection from="#a" to="#b" />
+      <Connection from="#b" to="#c" />
+      <Connection from="#c" to="#d" />
       <Node
+        id="a"
         icon="icons/Chrome.svg"
         title="Browser"
         description="Request to *.eugen-bondarev.com"
       />
       <Node
+        id="b"
         icon="icons/Google.svg"
         title="Google Cloud DNS"
         description="Traffic is redirected to 143.42.223.193"
+        // className="ml-[200px]"
       />
       <Node
+        id="c"
         icon="icons/Linode.svg"
         title="Linode Load Balancer"
         description="Traffic is redirected to ingress inside the k8s cluster"
       />
       <Node
+        id="d"
         icon="icons/Ingress.svg"
         title="Ingress"
         description="Traffic is redirected to one of the subdomains of eugen-bondarev.com"
