ed717b27b017f32eff0e3b745d56db5d11825fb8
Eugen Bondarev <92227280+eugen-bondarev@users.noreply.github.com>
2023-08-02 23:18:18 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ import { CanvasWidget } from '@projectstorm/react-canvas-core'
 import { useMemo } from 'react'
 
 // When new link is created by clicking on port the RightAngleLinkModel needs to be returned.
-export class RightAnglePortModel extends DefaultPortModel {
+class RightAnglePortModel extends DefaultPortModel {
   createLinkModel(factory?: AbstractModelFactory<LinkModel>) {
     return new RightAngleLinkModel()
   }
