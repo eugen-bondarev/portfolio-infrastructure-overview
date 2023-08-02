8050243ac3cb905731d36b9c02f1a415f4379c05
Eugen Bondarev <eug.bondarev@gmail.com>
2023-08-02 22:35:15 +0200

    Still integrating diagrams

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,108 @@
 'use client'
 
 import createEngine, {
+  AbstractModelFactory,
   DefaultLinkModel,
   DefaultNodeModel,
+  DefaultPortModel,
   DiagramModel,
+  LinkModel,
+  RightAngleLinkFactory,
+  RightAngleLinkModel,
 } from '@projectstorm/react-diagrams'
 
 import { CanvasWidget } from '@projectstorm/react-canvas-core'
+import { useMemo } from 'react'
 
-const engine = createEngine()
+// When new link is created by clicking on port the RightAngleLinkModel needs to be returned.
+export class RightAnglePortModel extends DefaultPortModel {
+  createLinkModel(factory?: AbstractModelFactory<LinkModel>) {
+    return new RightAngleLinkModel()
+  }
+}
 
-const node1 = new DefaultNodeModel({
-  name: 'Node 1',
-  color: 'rgb(0,192,255)',
-})
-node1.setPosition(100, 100)
-let port1 = node1.addOutPort('Out')
+interface Node {
+  label: string
+  id: string
+  position: [number, number]
+}
 
-const node2 = new DefaultNodeModel({
-  name: 'Node 1',
-  color: 'rgb(0,192,255)',
-})
-node2.setPosition(100, 100)
-let port2 = node2.addOutPort('Out')
+interface Edge {
+  target: string
+  source: string
+}
 
-const link = port1.link<DefaultLinkModel>(port2)
-link.addLabel('Hello World!')
+interface Graph {
+  nodes: Node[]
+  edges: Edge[]
+}
 
-const model = new DiagramModel()
-model.addAll(node1, node2, link)
-engine.setModel(model)
+const graph: Graph = {
+  nodes: [
+    {
+      id: '1',
+      label: 'Node 1',
+      position: [100, 100],
+    },
+    {
+      id: '2',
+      label: 'Node 2',
+      position: [400, 400],
+    },
+  ],
+  edges: [
+    {
+      target: '1',
+      source: '2',
+    },
+  ],
+}
+
+const createMyEngine = (graph: Graph) => {
+  const engine = createEngine()
+  engine.getLinkFactories().registerFactory(new RightAngleLinkFactory())
+
+  const nodes = graph.nodes.map((nodeDescriptor) => {
+    const node = new DefaultNodeModel({
+      name: nodeDescriptor.label,
+      color: 'rgb(0, 192, 255)',
+    })
+    node.setPosition(nodeDescriptor.position[0], nodeDescriptor.position[1])
+    const inPort = node.addPort(new RightAnglePortModel(true, 'in-1', 'In'))
+    const outPort = node.addPort(new RightAnglePortModel(false, 'out-1', 'Out'))
+    return {
+      object: node,
+      id: nodeDescriptor.id,
+      inPort,
+      outPort,
+    }
+  })
+
+  const links = graph.edges
+    .map((edgeDescriptor) => {
+      const node1ID = edgeDescriptor.source
+      const node1 = nodes.find((node) => node.id === node1ID)
+
+      const node2ID = edgeDescriptor.target
+      const node2 = nodes.find((node) => node.id === node2ID)
+
+      if (!node1 || !node2) {
+        return
+      }
+      return node1.outPort.link<DefaultLinkModel>(node2.inPort)
+    })
+    .filter(Boolean) as DefaultLinkModel[]
+
+  const model = new DiagramModel()
+  const nodeObjects = nodes.map(({ object }) => object)
+  model.addAll(...nodeObjects, ...links)
+  engine.setModel(model)
+  return engine
+}
 
 const Page = () => {
+  const engine = useMemo(() => createMyEngine(graph), [])
+
   return (
     <div className="canvas-container">
       <CanvasWidget engine={engine} />
